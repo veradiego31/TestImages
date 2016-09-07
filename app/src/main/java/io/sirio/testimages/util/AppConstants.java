ca45ab3f2d4d7223f8f915f944892b7b37cab478
Diego <veradiego31@gmail.com>
2016-09-07 10:21:40 -0400

    add icon launcher

--- app/src/main/java/io/sirio/testimages/util/AppConstants.java
+++ app/src/main/java/io/sirio/testimages/util/AppConstants.java
@@ -14,7 +14,7 @@ public class AppConstants {
     public static String YOUTUBE_URL = "http://www.youtube.com/channel/UCc8f2W4dVUYjlOm8wZYYcLg";
     public static String YOUTUBE_PAGE_ID = "UCc8f2W4dVUYjlOm8wZYYcLg";
 
-    public static final String TEST_DRIVE_URL = "http://eventostoyota.com.ar/form_testdrive/?origin=NzdYb2hCU3plTnFYdU1HWnZaQXFuWGFsOEUvS3NldldWVGZmbHlmVi9KK3ZpT3JHMTcrT2k5TEpPVzBDeDZDZnhoV1hTNG41ejFjMGx5eUpHOEJBYUE9PQ==&_ga=1.146137268.1624771937.1447870058";
+    public static final String TEST_DRIVE_URL = "http://www.google.com";
 
 
 }
